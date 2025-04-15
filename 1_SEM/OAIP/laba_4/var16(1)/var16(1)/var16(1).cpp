783f7a8fa6d2a35bdb9d906d214fe69a5aafbb84
Alexei <antipovalexei23@gmail.com>
2025-04-16 02:49:38 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_4/var16(1)/var16(1)/var16(1).cpp
@@ -0,0 +1,25 @@
+﻿#include <iomanip>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	int n = 40, k = 7;
+	char c, probel; probel = ' ';
+	cout << "Введите символ "; cin >> c;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << setw(n) << setfill(probel) << probel;
+		cout << setw(k) << setfill(c) << c << endl;
+		n = n - 2;
+		k = k + 4;
+	}
+	n = 42;
+	for (int i = 0; i < 8; i++)
+	{
+		cout << setw(n) << setfill(probel) << probel;
+		cout << setw(3) << setfill(c) << c << endl;
+	}
+
+}
---
+++ 1_SEM/OAIP/laba_4/var16(1)/zadan4/zadan4.cpp
@@ -0,0 +1,16 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	char name[60];
+	puts("Как вас зовут? ");
+	gets_s(name);
+	printf("Привет, %s\n", name," ! ");
+	printf("Чем занимаешься , %s\n", name, " ?");
+	gets_s(name);
+	printf("0_0 нууу удач тебе ");
+}
