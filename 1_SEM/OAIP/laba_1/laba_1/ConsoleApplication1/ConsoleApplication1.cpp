e44cd9ca5fc2050793c0b328816ba6fce6f32d76
Alexei <antipovalexei23@gmail.com>
2025-04-16 02:35:38 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_1/laba_1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_CTYPE,"Russian");
+    int x, y, z, n;
+    std::cout << "Введите x,y,z" << std::endl;
+    std::cin >> x;
+    std::cin >> y;
+    std::cin >> z;
+    if (x < y) {
+        n = x;
+        x = y;
+        y = n;
+    }
+    if (x < z) {
+        n = x;
+        x = z;
+        z = n;
+    }
+    if (y < z) {
+        n = y;
+        y = z;
+        z = n;
+    }
+    std::cout << x << y << z << std::endl;
+//вар1
+}
