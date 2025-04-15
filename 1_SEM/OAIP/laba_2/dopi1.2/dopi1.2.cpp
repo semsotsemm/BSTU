5aa1b93b8c506b86c8f8942829d2c525f6d13ec5
Alexei <antipovalexei23@gmail.com>
2025-04-16 02:37:14 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_2/dopi1.2/dopi1.2.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+string fileName;
+
+void writeToFile(const string& line)
+{
+    cout << "Введите название файла: ";
+    cin >> fileName;
+    FILE* file1;
+    if (fopen_s(&file1, fileName.c_str(), "w") != 0 || file1 == nullptr)
+    {
+        cout << "Ошибка при открытии файла!\n";
+        return;
+    }
+    fprintf(file1, "%s\n", line.c_str());
+    cout << "Строка записана в файл " << fileName << "\n";
+    fclose(file1);
+}
+
+void readLine(string& line)
+{
+    ifstream file(fileName);
+    if (!file)
+    {
+        cerr << "Ошибка открытия файла\n";
+        return;
+    }
+    getline(file, line);
+    cout << "Строка успешно считана\n";
+    file.close();
+}
+
+void getOddNumbers(string buf) 
+{
+    int number = 0;
+    bool getNumber = false;
+    for (int i = buf.length(); i > 0; i--)
+    {
+        for (int j = i, k = 0; buf[j] >= '0' && buf[j] <= '9' && j > 0; j-- , k++)
+        {
+            number += (buf[j] - '0') * pow(10, k);
+            getNumber = true;
+            i = j;
+        }
+        if (getNumber && number % 2 == 1)
+        {
+            cout << number << " ";
+        }
+        getNumber = false;
+        number = 0;
+    }
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    cout << "Введите строку: ";
+    string line;
+    getline(cin, line);
+    writeToFile(line);
+    string buf;
+    readLine(buf);
+    int number;
+    cout << "Нечетные числа из файла: ";
+    getOddNumbers(buf);
+}
---
+++ 1_SEM/OAIP/laba_2/dopi1/dopi1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void copyoddstrok(FILE* file1, ofstream& file2)
+{
+    char strok[256] = "";
+    int i = 0;
+    while (true)
+    {
+        fgets(strok, 256, file1);
+        if (feof(file1))
+        {
+            break;
+        }
+        i++;
+        if (i % 2 == 0)
+        {
+            file2 << strok;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    FILE* file1;
+    fopen_s(&file1, "FILE1.txt", "r");
+    ofstream file2("FILE2.txt");
+    int sizef1, sizef2;
+    copyoddstrok(file1, file2);
+    file2.close();
+    fseek(file1, 0L, SEEK_END);
+    sizef1 = ftell(file1);
+    fseek(file1, 0L, SEEK_SET);
+    fclose(file1);
+    cout << "Копирование строк произошло успешно, размер файла1 в байтах(учитывая что 1 символ = 1 байт) = " << sizef1;
+    fopen_s(&file1, "FILE2.txt", "r");
+    fseek(file1, 0L, SEEK_END);
+    sizef2 = ftell(file1);
+    fseek(file1, 0L, SEEK_SET);
+    fclose(file1);
+    cout << ", размер файла2 =" << sizef2;
+    return 0;
+}
