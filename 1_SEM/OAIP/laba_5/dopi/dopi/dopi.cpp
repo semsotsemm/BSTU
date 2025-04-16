7c2649c6f887e15c30ac2886abdbbddf39f67234
Alexei <antipovalexei23@gmail.com>
2025-04-16 03:05:40 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_5/dopi/dopi/dopi.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	int k;
+	puts("Хотите ли вы купить квартиру? (1 - да, 2 - нет)");
+	std::cin >> k;
+	switch (k)
+	{
+	case 1: {
+		puts("В каком городе (1- Минск, 2-Гродно, 3-Могилев)");
+		std::cin >> k;
+		switch (k)
+		{
+		case 1: puts("... удачи"); break;
+		case 2: puts("Хороший выбор"); break;
+		case 3: puts("Сомнительный выбор"); break;
+		}
+		break;
+	}
+	case 2: puts("Правильно, в общаге веселей"); break; default: puts("Некорректный вариант"); break;
+	}
+	return 0;
+}
---
+++ 1_SEM/OAIP/laba_5/dopi/dopi7.1/dopi7.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a, b, c,sum;
+    setlocale(LC_CTYPE, "Russian");
+    using namespace std;
+    cout << "Введите a,b,c" << endl;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "c = ";
+    cin >> c;
+    if (a%5 != 0 && b%5 != 0 && c%5 != 0)
+    {
+        cout << "Error"; 
+    }
+    if (a % 5 != 0)
+    {
+        a = 0;
+    }
+    if (b % 5 != 0)
+    {
+        b = 0;
+    }
+    if (a % 5 != 0)
+    {
+        c = 0;
+    }
+    sum = a + b + c;
+    cout << "Сумма чисел, кратнях 5 : " << sum;
+}
