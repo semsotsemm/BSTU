7c32d0f97adbc7903b86bcd91ed477ccf0e10f03
Alexei <antipovalexei23@gmail.com>
2025-04-16 03:35:38 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_9/Antipov_l.r-9/Antipov_l.r-9.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	int n = 200;
+	double a, b,h,x,s=0;
+	cout << "Введите значение переменной а ";
+	cin >> a;
+	cout << "Введите значение переменной b ";
+	cin >> b;
+	h = (b - a) / n; x = a;
+	for (int i = 0; i < 200; i++)
+	{
+		s = s + h * (pow(x,3)-3+pow((x+h),3)-3) / 2;
+		x = x + h;
+	}
+}
---
+++ 1_SEM/OAIP/laba_9/dopi2/dopi2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+double	f(double x) {
+	return pow(x,3) + 2*x -7;
+}
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	double fa = 1, fb = 1, x = 100, step = 0.1, e = 0.0001, x1, a, b;
+	while (fb * fa > 0)
+	{
+		fa = f(x);
+		fb = f(x - step);
+		if (fb * fa > 0)
+		{
+			x = x - step;
+		}
+	}
+	cout << "Отрезок на котором находится корень [ " << x - step << "; " << x << " ]" << endl;
+	a = x; b = x - step;
+	while (abs(a - b) > 2 * e)
+	{
+		x1 = (a + b) / 2;
+		if (f(x1) * f(a) <= 0)
+		{
+			b = x1;
+		}
+		else {
+			a = x1;
+		}
+	}
+	cout << "x = " << x1;
+}
