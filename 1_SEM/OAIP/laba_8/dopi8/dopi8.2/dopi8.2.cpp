cf55ce3ca2d90df48c121cd9e9e53235e928b850
Alexei <antipovalexei23@gmail.com>
2025-04-16 03:34:03 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_8/dopi8/dopi8.2/dopi8.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	int i = 1, xmin=0,imin,xmax=0,imax,result;
+	double x = 1;
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	cout << "Введите значения переменной x, закончите ввод цифрой 0 " << endl;
+	while (x != 0)
+	{
+		cout << "х" << i << " = ";
+		cin >> x;
+		if (x > xmin)
+		{
+			xmin = x;
+			imin = i;
+		}
+		if (x < xmax)
+		{
+			xmax = x;
+			imax = i;
+		}
+		i++;
+	}
+	result = abs(imin - imax);
+	cout << "Количество элеметнов между максимальным и минимальным: " <<result-1;
+}
---
+++ 1_SEM/OAIP/laba_8/dopi8/dopi8/dopi8.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	int x=1,i=1, sum=0;
+	using namespace std;
+	setlocale(LC_CTYPE ,"Russian");
+	cout << "Введите значения переменной x, закончите ввод цифрой 0 " << endl;
+	while (x!=0)
+	{
+		cout << "х" << i << " = ";
+		cin >> x;
+		if (x%2==0)
+		{
+			sum += x;
+		}
+		i++;
+	}
+	cout << "Сумма четных элементов массива = " << sum;
+}
