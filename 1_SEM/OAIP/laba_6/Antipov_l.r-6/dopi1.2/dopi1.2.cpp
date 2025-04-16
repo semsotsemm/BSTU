ad52964b316b7edf0ce59fb1ba461d132e09afcb
Alexei <antipovalexei23@gmail.com>
2025-04-16 03:14:16 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_6/Antipov_l.r-6/dopi1.2/dopi1.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	using namespace std;
+	float h, y, r = 4e-4, j=0, c = 2.1, m = 7;
+	setlocale(LC_CTYPE, "Russian");
+     while (j < 1.8)
+	{ 
+		h = (10 * r - j) / (pow(c, 2) + exp(-m));
+		y = (h * m - pow(j, 2)) + pow(0.1 * c, 2);
+		cout << "Если j = " << j << " h = "<< h << " ,y = " << y << endl;
+		j = j + 0.1;
+	}
+}
---
+++ 1_SEM/OAIP/laba_6/Antipov_l.r-6/dopi1/dopi1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	float h, y, r =4e-4, j, c=2.1, m = 7;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Введите переменную j ";
+		cin >> j;
+		h = (10 * r - j) / (pow(c, 2) + exp(-m));
+		y = (h * m - pow(j, 2)) + pow(0.1 * c, 2);
+		cout << "h = " << h << endl;
+		cout << "y = " << y << endl;
+	}
+}
