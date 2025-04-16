a596b1735e8627607f14b204646bf90c4e1da882
Alexei <antipovalexei23@gmail.com>
2025-04-16 10:35:58 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_12/dopi1.2/dopi1.2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE,"Russian");
+	int a[10], b[10], maxint=-2,n=-1;
+	cout << "Массив а: ";
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{   *(a + i) = rand() % 10;
+		*(b + i) = rand() % 10;
+		cout << *(a + i) << " ";
+	}
+	cout <<endl<< "Массив b: ";
+	for (int i = 0; i < 10; i++)
+	cout << b[i]<<" ";
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (*(a + i) == *(b + j))
+			n = *(a + i);
+		}
+		if (n==-1 && maxint<n)
+		{
+			maxint = *(a + i);
+		}
+		n = -1;
+	}
+	cout <<endl<< "Максимальный элемент массива а, которого нет в элементе б: " << maxint;
+}
---
+++ 1_SEM/OAIP/laba_12/dopi1/dopi1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int maxsize = 100;
+    setlocale(LC_CTYPE,"Russian");
+    int N, m,n,size=0,N1, crat[maxsize],j=0;
+    cout << "Введите целое число N "; cin >> N;
+    N1 = N;
+    cout << "Введите целое число m "; cin >> m;
+    while (N1!=0)
+    {
+        size++;
+        N1 /= 10;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        n = N % 10;
+        if (n % m == 0)
+        {
+            *(crat + j) = n;
+            j++;
+        }
+        N /= 10; 
+    }
+    for (int i = 0; i < j; i++)
+    {
+        cout << *(crat + i) << " ";
+    }
+}
