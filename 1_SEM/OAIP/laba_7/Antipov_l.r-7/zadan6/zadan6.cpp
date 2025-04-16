3bf81fa48c05b095b067ab9789e546b13e61d940
Alexei <antipovalexei23@gmail.com>
2025-04-16 03:26:16 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_7/Antipov_l.r-7/zadan6/zadan6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+    using namespace std;
+    setlocale(LC_CTYPE, "Russian");
+    double y, z, a = 1.774,x=3;
+    int m = 5;
+    while (x>=3 && x<=5)
+    {
+        y = pow((a + m * m * x * x),(0.5))/(a+x)*m;
+        if (abs(y)<1)
+        {
+            z = y + 1;
+        }
+        else if (abs(y)>=1)
+        {
+            z= sin(y)*sin(y);
+        }
+        x = x + 0.2;
+        cout << "z = " << z<< endl;
+    }
+}
---
+++ 1_SEM/OAIP/laba_7/Antipov_l.r-7/zadan7/zadan7.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+int main()
+{
+    using namespace std;
+    setlocale(LC_CTYPE, "Russian");
+    string a = "100";
+    int b = stoi(a);
+    while (b<999)
+    {
+        a = to_string(b);
+        if (a[0] != a[1] && a[1] != a[2] && a[2] != a[0])
+        {
+            cout << a << "\t";
+
+        }
+        b++;
+    }
+}
