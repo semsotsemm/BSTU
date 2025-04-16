87a7607651a2d21a4c6586059c92d5b5a05b9c8c
Alexei <antipovalexei23@gmail.com>
2025-04-16 03:08:47 +0300

    Update Antipov_l.r-5.cpp

--- laba_5/Antipov_l.r-5/Antipov_l.r-5/Antipov_l.r-5.cpp
+++ laba_5/Antipov_l.r-5/Antipov_l.r-5/Antipov_l.r-5.cpp
@@ -1,15 +1,1 @@
-﻿#include <iostream>
-void main()
-{
-	setlocale(LC_CTYPE, "Russian");
-	using namespace std;
-	double b, z, x = 4.e-3, s = 1.1, j;
-	cout << "Введите j ";
-	cin >> j;
-	b = s + (5 * x + j);
-	if (b < 1.5)
-		z = sqrt(x + 0.3 * j) + b;
-	else
-		z = abs(x * j + b);
-	cout << "j=" << j << " z=" << z << endl;
-}
+
