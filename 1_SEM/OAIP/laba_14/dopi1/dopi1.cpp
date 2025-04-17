b1689cd2c398018df117b91478bf3a946ad58b89
Alexei <antipovalexei23@gmail.com>
2025-04-17 09:49:27 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_14/dopi1/dopi1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	const int n=2;
+	int A[2*n][2*n],k;
+	srand((unsigned)time(NULL));
+	cout << "Матрица: ";
+	for (int i = 0; i < 2*n; i++)
+	{
+		cout << "\n";
+		for (int j = 0; j < 2*n; j++)
+		{
+			A[i][j] = -10 + rand() %21;
+			cout << A[i][j]<<"\t";
+		}
+	}
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+		{
+			k = A[i][j];
+			A[i][j] = A[i+n][j+n];
+			A[i + n][j + n] = k;
+			k = A[i][j + n];
+			A[i][j + n] = A[i + n][j];
+			A[i + n][j] = k;
+		}
+	cout <<endl<< "Исходная матрица: ";
+	for (int i = 0; i < 2 * n; i++)
+	{
+		cout << "\n";
+		for (int j = 0; j < 2 * n; j++)
+			cout << A[i][j] << "\t";
+	}
+}
---
+++ 1_SEM/OAIP/laba_14/dopi3/dopi3.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	int n,maxA=0,num= 0;
+	cout << "Введите размер массива: ";
+	cin >> n;
+	int* max = new int[n];
+	int** A = new int* [n];
+	for (int i = 0; i < n; i++)
+	{
+		A[i] = new int[n];
+	}
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			A[i][j] = rand() % 100;
+			cout << A[i][j]<<"\t";
+		}
+		max[i] = 1000;
+		cout << endl;
+	}
+	for (int k = 0; k < n; k++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (k==0&& A[i][j] > num)
+				{
+					num = A[i][j];
+				}
+				else if (A[i][j] > num && A[i][j]<max[k-1])
+				{
+					num = A[i][j];
+				}
+			}
+		}
+		max[k] = num;
+		maxA = num;
+		num = 0;
+	}
+	cout << "Исходная матрица:\n";
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) 
+		{
+			if (i == j)
+			{
+				int r = A[i][j];
+				for (int b = 0; b < n; b++)
+				{
+					for (int c = 0; c < n; c++)
+					{
+						if (A[b][c]==max[i])
+						{
+							A[b][c] = r;
+						}
+					}
+				}
+				A[i][j] = max[i];
+			}
+			cout << A[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
