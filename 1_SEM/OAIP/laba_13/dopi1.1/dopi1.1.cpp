fe067506561d097b1725874552eec5e42463d868
Alexei <antipovalexei23@gmail.com>
2025-04-17 09:38:40 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_13/dopi1.1/dopi1.1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char strok[256],past[256];
+    int size=0,n,k,b;
+    setlocale(LC_CTYPE,"Russian");
+    cout << "Введите строку "; cin >> strok;
+    cout << "Введите количество символов которых хотите вставить в строку (n) "; cin >>n;
+    cout << "Введите какие символы хотите вставить "; cin >> past;
+    cout << "Введите начиная с какой позиции хотите вставить n символов (k) "; cin >> k;
+    for (int i = 0; i < 256; i++)
+    {
+        if (strok[i] == '\0')
+            break;
+        else
+            size++;
+    }
+    for (int i = size; i > 1 ; i--)
+    {
+        b = i-1;
+        for (int j = 0; j < n; j++)
+        {
+            strok[b+1] = strok[b];
+            b++;
+        }
+    }
+    for (int i = 0; i < n; i++)
+        strok[i + k-1] = past[i];
+    strok[size + n] = '\0';
+    cout << strok;
+}
---
+++ 1_SEM/OAIP/laba_13/dopi1/dopi1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	char words[] = "Лабараторная, практическое, техническое, проеголв"; //Заданная строка 
+	char end[] = "ое";// окончание
+	int a= strlen(end),n=0;	
+		for (int i = strlen(words); i >0; i--)
+		{
+			for (int k = 1; k <= a; k++)
+				if ((end[a-k]==words[i-k])&& (words[i]=='\0'||words[i]==','))
+					n++;
+			if (n==a)
+			{
+				int j = i;
+				while (words[j] != ' ' || j == 0)
+					j--;
+				while (j!=i)
+					cout << words[++j];
+				cout << ' ';
+				n = 0;
+			}
+		}
+}
