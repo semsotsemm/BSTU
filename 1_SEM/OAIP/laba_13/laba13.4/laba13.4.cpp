0e6c480b9a95a9a35481740c466e9bff1de3f60c
Alexei <antipovalexei23@gmail.com>
2025-04-17 09:46:52 +0300

    Add files via upload

---
+++ 1_SEM/OAIP/laba_13/laba13.4/laba13.4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+		char t[3][20];
+		int i, n;
+		cout << "Input size (<=3): ";
+		cin >> n;
+		//В буфере остается /n
+		//cin.ignore(cin.rdbuf()->in_avail());
+		for (i = 0; i < n; i++)
+		{
+			cout << "Input string: ";
+			// /n считывается как пустая строка 
+			gets_s(t[i]);
+		}
+		for (i = 0; i < n; i++)
+			puts(t[i]);
+}
---
+++ 1_SEM/OAIP/laba_13/laba13.5/laba13.5.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+void main()
+{
+	char s[256];
+	int i, count;
+	puts("Enter string: ");
+	gets_s(s);
+	for (count = i = 0; s[i] != 0; i++)
+	{
+		if (s[i] == '(') count++;
+		if (s[i] == ')') count--;
+	}
+	if (!count)
+	for (count = i = 0; s[i] != 0; i++)
+	{
+		if (s[i] == '(')
+		{
+			count++;
+			for (int j = i+1; s[j] != 0; j++)
+				if (s[j] == ')')
+				{
+					count--;
+					break;
+				}
+		}
+	}
+	if (!count)
+		puts("Ok\n");
+	else
+		puts("Not Ok\n");
+}
